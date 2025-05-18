71d668817387354295ee6c469c73a1a5ec53946b
David Carr <dave@dcblog.dev>
2025-05-18 14:36:40 +0100

    Add Xero dashboard component for admin panel
    Introduce a new Livewire component, `XeroDashboard`, along with its corresponding Blade view. This provides a basic interface indicating connection to Xero.

---
+++ app/Livewire/Admin/Xero/XeroDashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Livewire\Admin\Xero;
+
+use Livewire\Component;
+
+class XeroDashboard extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.xero.xero-dashboard');
+    }
+}
---
+++ resources/views/livewire/admin/xero/xero-dashboard.blade.php
@@ -0,0 +1,3 @@
+<div>
+    <h1>Connected to Xero.</h1>
+</div>
