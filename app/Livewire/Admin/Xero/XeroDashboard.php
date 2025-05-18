398bbdea2ba2b3ba8f45e661f4053bf9987c870c
David Carr <dave@dcblog.dev>
2025-05-18 14:37:21 +0100

    Add type hint for render method in XeroDashboard component

--- app/Livewire/Admin/Xero/XeroDashboard.php
+++ app/Livewire/Admin/Xero/XeroDashboard.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace App\Livewire\Admin\Xero;
 
+use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class XeroDashboard extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire.admin.xero.xero-dashboard');
     }
